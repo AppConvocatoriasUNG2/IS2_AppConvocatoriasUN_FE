0ce44be32ebba8592dba2e67bedc8cb394c3732a
Freddy <facuellarg@unal.edu.co>
2018-04-05 16:22:38 -0500

    header and footer

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,12 +4,13 @@ import React, {Component} from 'react';
 export class Formulario extends Component{
 	constructor(props){
 		super(props);
-		this.state={login:1, email:' ', name:' '};
+		this.state={login:1, email:' ', password:' '};
 		this.handlerOnclickLogin = this.handlerOnclickLogin.bind(this);
 		this.handlerOnclickRegistro = this.handlerOnclickRegistro.bind(this);
-		this.handleOnSubmit = this.handleOnSubmit.bind(this)
-		this.handleInputChangeName = this.handleInputChangeName.bind(this)
-	}
+		this.handleOnSubmitLogin = this.handleOnSubmitLogin.bind(this);
+		this.handleInputChangeName = this.handleInputChangeName.bind(this);
+		this.handleOnSubmitRegistro = this.handleOnSubmitRegistro.bind(this);
+		}
 
 
 
@@ -28,10 +29,14 @@ export class Formulario extends Component{
 		})
 	}
 	handleInputChangeName(e){
-		 this.setState({name: e.target.value});
+
 	}
-	handleOnSubmit(e){
-		console.log(this.state.name)
+	handleOnSubmitLogin(e){
+		
+
+		console.log(this.state)
+		// const email = document.getElementById('email');
+		// const password = document.getElementById('password');
 		fetch('https://api.github.com/users')
 	    	.then((response) => {
 	    	  return response.json()
@@ -41,23 +46,101 @@ export class Formulario extends Component{
 	        
 	      })
 
-	      e.preventDefault()
+	      e.preventDefault();
 	}
+	async handleOnSubmitRegistro(e){
+
+		e.preventDefault();
+		const nombreCompleto = this.refs.nameR +" "+this.refs.lastNameR.value;
+		const email = this.refs.emailR.value;
+		const password = this.refs.passwordR.value;
+		const confPassword = this.refs.confPassword.value;
+		if(password !== confPassword){
+			alert("las contraseñas no coinciden");
+			return;
+		}
+
+		const options={
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+         },
+      body: JSON.stringify({
+          name: nombreCompleto,
+          email: email,
+          encryptedPassword: password,
+      	}) 
+      
+		}
+
+		try{
+			let response = await fetch('https://todoapirest.herokuapp.com/todos', options);
+			if(response.ok){
+				let jsonResponse = await response.json();
+				console.log(jsonResponse);
+				
+			}
+			throw new Error("FALOOO");
+		}catch(error){
+			console.log(error.message)
+
+		}
+		
+
+
+	}
+	
+		// const passwordR = document.getElementById('passwordR').value;
+		// const confPassword = document.getElementById('confPassword').value;
+		// try{
+		// 	let response = await fetch('https://todoapirest.herokuapp.com/todos' ,
+		// 					{
+  //             method: 'POST',
+  //             headers: {
+  //                 'Accept': 'application/json',
+  //                 'Content-Type': 'application/json',
+  //                },
+	 //            body: JSON.stringify({
+	 //                name: name+" "+lastName,
+	 //                description: emailR,
+	 //                duration: 10,
+	               
+	 //            })
+		// 	})
+		// 	if(response.ok){
+		// 		console.log("hay respuestaaa");
+		// 		e.preventDefault();
+		// 	}	throw new Error("fallo");
+
+
+		// }catch(error){
+		// 	console.log(error.message)
+
+		// }
+
+		
+		
+		
+
+
+	
 
 	render(){
+
 			if(this.state.login === 1){
 				return(
 					<div style={{width: '100'}}>
 						<div className="row">
 							<div id="formularios">
 								<div id="formularioSesion" >
-									<form style={{textAlign: 'center'}} id='formularioSesion' onSubmit={this.handleOnSubmit}>
+									<form style={{textAlign: 'center'}} id='formularioSesion' onSubmit={this.handleOnSubmitLogin}>
 										<div className="btn-group" style={{width: '100'}}>
 											<button id="botonIniciarSesion" type="button" onClick={this.handlerOnclickLogin} className="btn btn-info" style={{width: '50'}} >Iniciar sesion</button>
 											<button id="botonRegistro" type="button" className="btn btn-default" onClick={this.handlerOnclickRegistro}  style={{width: '50'}}>Registrarse</button><br/>
 						  			</div><br/>
-										<input className="inputInicio" type="text" name="correoInstitucional" placeholder="Correo institucional" onChange={this.handleInputChangeName}/><br/>				
-			 							<input className="inputInicio" type="text" name="contraseña" placeholder="Ingrese su contraseña"/><br/>
+										<input className="inputInicio" ref="email" type="text" name="correoInstitucional" placeholder="Correo institucional" /><br/>				
+			 							<input className="inputInicio" ref="password" type="password" name="contraseña" placeholder="Ingrese su contraseña" /><br/>
 										<input id="submitIniciarSesion" type="submit" value="Iniciar sesion"/>
 									</form>
 								</div>
@@ -72,16 +155,17 @@ export class Formulario extends Component{
 						<div id="formularios">
 						  	
 								<div id="formularioRegistro" >
-									<form style={{textAlign: 'center'}} method='post'>
+									<form style={{textAlign: 'center'}} onSubmit={this.handleOnSubmitRegistro}>
 										<div className="btn-group" >
 											<button id="botonIniciarSesion" type="button" onClick={this.handlerOnclickLogin} className="btn btn-info" style={{width: '50'}} >Iniciar sesion</button>
 											<button id="botonRegistro" type="button" className="btn btn-default" onClick={this.handlerOnclickRegistro}  style={{width: '50'}}>Registrarse</button><br/>
 						  			</div><br/>
-										<input className="inputna" type="text" name="nombres" placeholder="Nombres"/>
-										<input className="inputna" type="text" name="apellidos" placeholder="Apellidos"/><br/>
-					  				<input className="inputRegistro" type="text" name="correoInstitucional" placeholder="Correo institucional"/><br/>
-										<input className="inputRegistro" type="text" name="contraseña" placeholder="Ingrese su contraseña"/><br/>
-					 					<input className="inputRegistro" type="text" name="confirmarContraseña" placeholder="Confirmar Contraseña"/><br/>
+										<input className="inputna" type="text" ref="nameR"  placeholder="Nombres" />
+										<input className="inputna" type="text" ref="lastNameR" placeholder="Apellidos" /><br/>
+					  				<input className="inputRegistro" type="text" ref="emailR" placeholder="Correo institucional" /><br/>
+					  				<input className="inputRegistro" type="password" ref="passwordR" placeholder="Contraseña" /><br/>
+					  				<input className="inputRegistro" type="password" ref="confPassword" placeholder="Confirmar Contraseña" /><br/>
+										
 					 					<input className="inputRegistro" type="submit" value="Registrarse"/>
 									</form>
 			 					</div>
